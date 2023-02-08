47b8dc60b2a67a2e39f9252fec530cafe8fd74cc
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-08 14:15:06 +0330

    functions in js

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-function sayHello() {
-  console.log("hello");
+// ***** Function section *****
+
+// function sayHello() {
+//   console.log("hello");
+// }
+
+// sayHello();
+
+// function sayBye() {
+//   console.log("Bye");
+// }
+
+// sayBye();
+
+// function sing(song) {
+//   console.log(song);
+// }
+
+// sing("ladddddd");
+// sing("daaaaaaaaa");
+// sing("saaaaaaaaa");
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// alert(multiply(5, 10));
+
+// var multiply = function (a, b) {
+//   if (a >= 10 || b >= 10) {
+//     alert("The parameters high");
+//   } else {
+//     return a * b;
+//   }
+// };
+
+// let multi = multiply(4, 5);
+
+// alert(multi);
+function multiply(a, b) {
+  if (a > 10 || b > 10) {
+    alert("The parameters is to high");
+  } else {
+    return a * b;
+  }
 }
 
-sayHello();
-
-function sayBye() {
-  console.log("Bye");
-}
-
-sayBye();
+let total = multiply(5, 4);
+alert(total);
