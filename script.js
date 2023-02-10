eeb28495f440dc5cd1dc4135cf8e9066beb1c541
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-10 20:16:36 +0330

    Create CheckDriveAge function in js

--- script.js
+++ script.js
@@ -37,13 +37,63 @@
 // let multi = multiply(4, 5);
 
 // alert(multi);
-function multiply(a, b) {
-  if (a > 10 || b > 10) {
-    alert("The parameters is to high");
-  } else {
-    return a * b;
+// function multiply(a, b) {
+//   if (a > 10 || b > 10) {
+//     alert("The parameters is to high");
+//   } else {
+//     return a * b;
+//   }
+// }
+
+// let total = multiply(5, 4);
+// alert(total);
+
+// function checkDriverAge(num) {
+//   if (num > 18) {
+//     console.log("Hi Congratulations you to enjoy to drive Power On ");
+//   } else if (num < 18) {
+//     console.log("Sorry, You are to young to drive this car. Power OFF");
+//   }
+// }
+// var chek = checkDriverAge();
+// let chekPrompt = prompt(chek);
+
+// function checkDriverAge() {
+//   var age = Number(prompt("What's your Old?"));
+
+//   if (age === 18) {
+//     alert("Hi Congratulations you to enjoy to drive Power On");
+//   } else if (age > 18) {
+//     alert("enjoy your Drive Powe On");
+//   } else if (age < 18) {
+//     alert("Sorry, You are to young to drive this car. Power OFF");
+//   }
+// }
+
+// checkDriverAge();
+
+// var checkDriveAge2 = function () {
+//   let age = Number(prompt("what's your old?"));
+
+//   if (age === 18) {
+//     alert("Hi Congratulations you to enjoy to drive Power On");
+//   } else if (age > 18) {
+//     alert("Enjoy your Drive Powe on");
+//   } else if (age < 18) {
+//     alert("Sorry, You are to young to drive this car. Power OFF");
+//   }
+// };
+
+// checkDriveAge2();
+
+function checkDriveAge(age) {
+  if (Number(age) === 18) {
+    return "Hi Congratulations you to enjoy to drive Powe On";
+  } else if (Number(age) > 18) {
+    return "Enjoy you ride Pwoer On";
+  } else if (Number(age) < 18) {
+    return "Sorry, you are to young to drive this car. Power Off";
   }
 }
 
-let total = multiply(5, 4);
-alert(total);
+// checkDriveAge();
