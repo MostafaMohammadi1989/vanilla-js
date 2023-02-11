d96a199b053236da41a2737033832b283493075e
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-12 01:45:25 +0330

    Object in Data structures

--- dataStructure.js
+++ dataStructure.js
@@ -1,3 +1,4 @@
+// ****** ARRAY *****
 // var list = ["tiger", "cat", "bear", "monky"];
 
 // console.log("Tiger: ", list[0]);
@@ -15,7 +16,7 @@
 // var myList = list.concat(["eggle"]);
 // console.log(myList);
 
-var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 // 1. Remove the Banana from the arry
 // 2. Sort the array in order
@@ -31,13 +32,44 @@ var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 // var array = ["Banana", ["Apples", ["Oranges",] "Blueberries"]]
 // access "oranges".
 
-var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
-console.log(array.shift());
-console.log(array.sort());
-console.log(array.push("Kiwi"));
-console.log(array.splice(0, 1));
-// console.log(array.indexOf("0"));
-console.log(array);
-console.log(array.reverse());
-console.log(array2[1][1][0]);
+// console.log(array.shift());
+// console.log(array.sort());
+// console.log(array.push("Kiwi"));
+// console.log(array.splice(0, 1));
+// // console.log(array.indexOf("0"));
+// console.log(array);
+// console.log(array.reverse());
+// console.log(array2[1][1][0]);
+
+// ***** OBJECT *****
+
+var myObj = {
+  name: "Meisam",
+  age: 33,
+  hobby: "Soccer",
+  isMarried: false,
+  shout: function () {
+    console.log("this is array in object");
+  },
+  animal: ["tiger", "monky", "elephent"],
+};
+
+// access to function in array
+// and acually we can access to array in object:
+// like this we want to access monky in array in myObj object:
+// myObj.animal[1] ==> monky
+// also we can use the bulite function in this array in object
+
+var list = [
+  "apples",
+  "Kiwi",
+  "Oranges",
+  {
+    name: "Majid",
+    lastName: "Rahimi",
+    hobby: "HichHik",
+    age: 35,
+  },
+];
