23d322c9343c35046e4ca8a2c9e012e1feda8a37
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-12 02:22:42 +0330

    Bulid basic facebook exercise

--- dataStructure.js
+++ dataStructure.js
@@ -45,16 +45,16 @@
 
 // ***** OBJECT *****
 
-var myObj = {
-  name: "Meisam",
-  age: 33,
-  hobby: "Soccer",
-  isMarried: false,
-  shout: function () {
-    console.log("this is array in object");
-  },
-  animal: ["tiger", "monky", "elephent"],
-};
+// var myObj = {
+//   name: "Meisam",
+//   age: 33,
+//   hobby: "Soccer",
+//   isMarried: false,
+//   shout: function () {
+//     console.log("this is array in object");
+//   },
+//   animal: ["tiger", "monky", "elephent"],
+// };
 
 // access to function in array
 // and acually we can access to array in object:
@@ -62,14 +62,77 @@ var myObj = {
 // myObj.animal[1] ==> monky
 // also we can use the bulite function in this array in object
 
-var list = [
-  "apples",
-  "Kiwi",
-  "Oranges",
+// var list = [
+//   "apples",
+//   "Kiwi",
+//   "Oranges",
+//   {
+//     name: "Majid",
+//     lastName: "Rahimi",
+//     hobby: "HichHik",
+//     age: 35,
+//   },
+// ];
+
+// Create an object and an array which we will use in our facebook exercise.
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+// {
+// 	username: "andrei",
+// 	password: "supersecret"
+// }
+
+// 2. Create an array which contains the object you have made above and name the arry "database"
+// var database = [
+//   {
+//     username: "andrei",
+//     password: "supersecret",
+//   },
+// ];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+// var newsfeed = [
+//   {
+//     username: "Bobby",
+//     timeline: "So tired from all that learning!",
+//   },
+//   {
+//     username: "Sally",
+//     timeline: "Javascript is sooooo cool!",
+//   },
+//   {
+//     username: "Mitch",
+//     timeline: "Javascript is preeetyy cool!",
+//   },
+// ];
+
+var dataBase = [
   {
-    name: "Majid",
-    lastName: "Rahimi",
-    hobby: "HichHik",
-    age: 35,
+    username: "Meisam",
+    password: "123",
   },
 ];
+
+var newsFeed = [
+  {
+    name: "Boby",
+    timeline: "This is a coool javascript video",
+  },
+  {
+    name: "Sully",
+    timeline: "Javascript is very cool",
+  },
+];
+
+var usernamePrompt = prompt("what's your username?");
+var passwordPrompt = prompt("what's your password?");
+
+function signIn(user, pass) {
+  if (user === dataBase[0].username && pass === dataBase[0].password) {
+    console.log(newsFeed);
+  } else {
+    alert("Sorry, wrong your username or password");
+  }
+}
+
+signIn(usernamePrompt, passwordPrompt);
