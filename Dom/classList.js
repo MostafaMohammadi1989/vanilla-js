0bd65dab0e8b0dd51bcb035873432a63f8a07acd
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-17 18:05:44 +0330

    Toggle method and use them in funtion

--- Dom/classList.js
+++ Dom/classList.js
@@ -8,3 +8,16 @@ function classNameH() {
     h1ClassName[0].className = "h1class";
   }
 }
+
+let classLi = document.querySelector("#liOne").classList;
+console.log(classLi);
+
+// let calssLi2 = document.querySelectorAll("#liOne");
+// calssLi2[2].classList;
+// console.log(calssLi2);
+
+let toggleBtn = function () {
+  let h1Toggle = document
+    .querySelector(".h1toggle")
+    .classList.toggle("h1ToggleClass");
+};
